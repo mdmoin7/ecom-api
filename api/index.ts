72acb4c88a5ba54f038959271d2bfa6d13e5e448
mdmoin7 <mohammad.nicoll@gmail.com>
2026-08-20 12:45:22 +0530

    refactor: remove database connection logic from index.ts and simplify export; update vercel.json with rewrites

--- api/index.ts
+++ api/index.ts
@@ -1,25 +1,3 @@
-import dotenv from "dotenv";
-import mongoose from "mongoose";
 import app from "../src/server/index.js";
 
-dotenv.config();
-
-let isConnected = false;
-
-async function connectDB() {
-  if (isConnected && mongoose.connection.readyState === 1) {
-    return;
-  }
-
-  if (!process.env.DB_STRING) {
-    throw new Error("DB_STRING is not defined");
-  }
-
-  await mongoose.connect(process.env.DB_STRING);
-  isConnected = true;
-}
-
-export default async function handler(req: any, res: any) {
-  await connectDB();
-  return app(req, res);
-}
+export default app;
